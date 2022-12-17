bb4edeaddab4b262b6ac7e8391562ae3d86800ec
Glendson <gztomazetto@gmail.com>
2022-12-17 22:02:45 +0000

    user cases finish

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,21 +1,45 @@
 import { Injectable } from "@nestjs/common";
-import { Notification } from "src/application/entities/notification";
-import { NotificationsRepository } from "../../../../application/repositories/notifications-repository";
+import { Notification } from "@application/entities/notification";
+import { NotificationsRepository } from "@application/repositories/notifications-repository";
 import { PrismaNotificationMapper } from "../mappers/prisma-notification-mapper";
 import { PrismaService } from "../prisma.service";
 
 @Injectable()
-export class PrismaNotificationsRepository implements NotificationsRepository{
+export class PrismaNotificationsRepository implements NotificationsRepository {
 
-    constructor(private prismaService: PrismaService){}
+    constructor(private prisma: PrismaService) { }
+
+    async findById(notificationId: string): Promise<Notification | null> {
+        const notification = await this.prisma.notification.findUnique({
+            where: { id: notificationId },
+        })
+
+        if(!notification){
+            return null
+        }
+
+        return PrismaNotificationMapper.toDomain(notification)
+    }
+
+    findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    countManyByRecipientId(recipientId: string): Promise<number> {
+        throw new Error("Method not implemented.");
+    }
+
 
     async create(notification: Notification): Promise<void> {
 
         const raw = PrismaNotificationMapper.toPrisma(notification)
 
-        await this.prismaService.notification.create({
+        await this.prisma.notification.create({
             data: raw,
         })
     }
 
+    async save(notification: Notification): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
 }
